13f890d83d51184a59232b1b817226c909d0060e
Rock26soft <74952168+Rock26soft@users.noreply.github.com>
2023-06-05 12:22:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/rock/tvguide/MainActivity.java
+++ app/src/main/java/com/rock/tvguide/MainActivity.java
@@ -1,109 +1,88 @@
-
 package com.rock.tvguide;
 
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackException;
+import com.google.android.exoplayer2.*;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.drm.DefaultDrmSessionManager;
-import com.google.android.exoplayer2.drm.DrmSessionManager;
-import com.google.android.exoplayer2.drm.FrameworkMediaDrm;
-import com.google.android.exoplayer2.drm.UnsupportedDrmException;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.trackselection.*;
+import com.google.android.exoplayer2.upstream.*;
+import com.google.android.exoplayer2.source.*;
+import com.google.android.exoplayer2.drm.*;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SimpleExoPlayer player;
-    private PlayerView playerView;
+  private SimpleExoPlayer player;
+  private PlayerView playerView;
 
-    private static final String DRM_SCHEME = "clearkey";
-    private static final UUID DRM_SCHEME_UUID = C.CLEARKEY_UUID;
+ 
 
-    // Sample ClearKey KID and Key
-    private static final String CLEARKEY_KID = "HL4pP9w4XayMDVArBPM45A";
-    private static final String CLEARKEY_KEY = "BiTNLoAjrsXatKgrICCq9w";
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    playerView = findViewById(R.id.player_view);
+  }
 
-        playerView = findViewById(R.id.player_view);
-    }
+  @Override
+  protected void onStart() {
+    super.onStart();
+    initializePlayer();
+  }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        initializePlayer();
-    }
+  @Override
+  protected void onStop() {
+    super.onStop();
+    releasePlayer();
+  }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releasePlayer();
-    }
+  private void initializePlayer() {
+    player =
+      new SimpleExoPlayer.Builder(this)
+        .setMediaSourceFactory(new DefaultMediaSourceFactory(headers()))
+        .build();
+    playerView.setPlayer(player);
+    DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
+    String vid ="https://bpprod7linear.akamaized.net/bpk-tv/irdeto_com_Channel_257/output/manifest.mpd";
 
-    private void initializePlayer() {
-        TrackSelector trackSelector = new DefaultTrackSelector();
-        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultRenderersFactory(this), trackSelector);
+    Uri videoURI = Uri.parse(vid);
 
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "ExoPlayerDemo"));
-        DrmSessionManager drmSessionManager;
-        try {
-            drmSessionManager = buildDrmSessionManager();
-        } catch (UnsupportedDrmException e) {
-            e.printStackTrace();
-            return;
-        }
+    String keyString =
+      "{\"keys\":[{\"kty\":\"oct\",\"k\":\"BiTNLoAjrsXatKgrICCq9w\",\"kid\":\"HL4pP9w4XayMDVArBPM45A\"}],'type':\"temporary\"}";
 
-        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
-                .setExtractorsFactory(new DefaultExtractorsFactory())
-                .createMediaSource(Uri.parse("https://bpprod7linear.akamaized.net/bpk-tv/irdeto_com_Channel_257/output/manifest.mpd"));
+    MediaDrmCallback drmCallback = new LocalMediaDrmCallback(
+      keyString.getBytes()
+    );
 
-        player.setPlayWhenReady(true);
-        player.prepare(mediaSource);
-        player.setDrmSessionManager(drmSessionManager);
-        player.addListener(new Player.DefaultEventListener() {
-            @Override
-            public void onPlayerError(PlaybackException error) {
-                super.onPlayerError(error);
-                // Handle player error
-            }
-        });
-
-        playerView.setPlayer(player);
-    }
-
-    private DrmSessionManager buildDrmSessionManager() throws UnsupportedDrmException {
-        FrameworkMediaDrm mediaDrm = new FrameworkMediaDrm(DRM_SCHEME_UUID);
-        
-        byte[] key = Util.getUtf8Bytes(CLEARKEY_KEY);
-        UUID keyId = UUID.fromString(CLEARKEY_KID);
-        
+    MediaSource dashMediaSource = new DashMediaSource.Factory(dataSourceFactory)
+      .setDrmSessionManagerProvider(mediaItem -> {
         return new DefaultDrmSessionManager.Builder()
-                .setUuidAndExoMediaDrmProvider(DRM_SCHEME_UUID, FrameworkMediaDrm.DEFAULT_PROVIDER)
-                .build(new DrmMediaDrmCallback(keyId, key));
-    }
+          .setPlayClearSamplesWithoutKeys(true)
+          .setMultiSession(false)
+          .setKeyRequestParameters(new HashMap<String, String>())
+          .setUuidAndExoMediaDrmProvider(
+            C.CLEARKEY_UUID,
+            FrameworkMediaDrm.DEFAULT_PROVIDER
+          )
+          .build(drmCallback);
+      })
+      .createMediaSource(MediaItem.fromUri(videoURI));
 
-    private void releasePlayer() {
-        if (player != null) {
-            player.release();
-            player = null;
-        }
+    player.setMediaSource(dashMediaSource);
+    player.setPlayWhenReady(true);
+    player.prepare();
+    player.play();
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      player.release();
+      player = null;
     }
+  }
 }
