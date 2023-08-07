03ec07789b82bb54a9c0709f25027bf794fce0c5
Rock26soft <74952168+Rock26soft@users.noreply.github.com>
2023-08-07 17:38:46 +0530

    Add files via upload

--- app/src/main/java/com/rock/tvguide/PlayerActivity.java
+++ app/src/main/java/com/rock/tvguide/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.rock.tvguide;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.exoplayer2.*;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
@@ -15,7 +16,7 @@ import com.google.android.exoplayer2.util.Util;
 public class PlayerActivity extends AppCompatActivity {
 
   private PlayerView playerView;
-  private SimpleExoPlayer player;
+  private ExoPlayer player;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +25,18 @@ public class PlayerActivity extends AppCompatActivity {
 
     playerView = findViewById(R.id.playerView);
 
-    player = new SimpleExoPlayer.Builder(this).build();
+    player = new ExoPlayer.Builder(this).build();
 
     playerView.setPlayer(player);
 
     Uri uri = Uri.parse(getIntent().getStringExtra("url"));
-    DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
-      this,
-      Util.getUserAgent(this, "ExoPlayerDemo")
-    );
-    MediaSource mediaSource = new ProgressiveMediaSource.Factory(
-      dataSourceFactory
-    )
-      .createMediaSource(uri);
-
-    player.prepare(mediaSource);
-
-    player.setPlayWhenReady(true);
+    MediaItem mediaItem = MediaItem.fromUri(videoUri);
+    // Set the media item to be played.
+    player.setMediaItem(mediaItem);
+    // Prepare the player.
+    player.prepare();
+    // Start the playback.
+    player.play();
   }
 
   @Override
